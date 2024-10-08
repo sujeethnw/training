fff4a963644be1bd1f3d6a8f9588885915a59972
sujeethnw <sujeeth.kumar@nativewit.in>
2024-10-08 15:45:43 +0530

    collections

--- lib/programsforpractice.dart
+++ lib/programsforpractice.dart
@@ -1,14 +1,68 @@
 import 'dart:collection';
+//import 'dart:nativewrappers/_internal/vm/lib/internal_patch.dart';
 void main() {
-  var list = ['1', '2', '3'];
-  print(list);
+  /*var lists = ['1', '2', '3'];
+  print(lists);
   var set={'hi','we','are','weak'};
 print(set);
   var sets= <String>{};
    sets.add('hi');
    sets.add('hello');
   print(sets);
-  print((sets.length==9));
+  print((sets.length==8));
+  var vari=<String,String>{};
+  vari['first']='hi';
+  print(vari);
+  var gifts = Map<String, String>();
+  gifts['first'] = 'partridge';
+  gifts['second'] = 'turtledoves';
+  gifts['fifth'] = 'golden rings';
+print(gifts);
+  var Gas = Map<int, String>();
+  Gas[2] = 'helium';
+  Gas[10] = 'neon';
+  Gas[18] = 'argon';
+  print(Gas);*/
+/*List<String> list=[];
+list.add('hi');
+list.add('2');
+list.add('sk');
+List<int>ad=[];
+ad.add(1);
+ad.add(2);
+List<String> b = ad.map((e)=> e.toString()).toList();
+print(list + b);
+var l=[];
+l.addAll([10*9,"2 String"]);
+print(l);
+List<String> sk =[];
+sk.add('hi');
+sk.add('bye');
+print(sk);
+List<String> sh=[];
+sh.add('ok');
+sh.add('no');
+print(sh);
+List a=sk+sh;
+print(a);*/
+
+/*var lists=['hi','ok','ds','sw'];
+lists.remove(0);
+lists.remove(1);
+//lists.add('hi');
+print(lists[0]);*/
+/*Map<String,int> op={'apple':1,'orange':2};
+Map<int,String>no={1:'mang',2:'berr',3:'van'};
+no.addAll({4:'banana'});
+no[4]='straw';
+print(no.remove(3));
+print(no);
+print(op['apple']);
+var map3={};
+map3.addAll(op);
+map3.addAll(no);
+print(map3);*/
+
 
 
 
