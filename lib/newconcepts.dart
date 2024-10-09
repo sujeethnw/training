251a5762dc9d9ac0bcb7b6d4872ef6119e2e5af1
sujeethnw <sujeeth.kumar@nativewit.in>
2024-10-09 13:04:19 +0530

    mixin

---
+++ lib/newconcepts.dart
@@ -0,0 +1,55 @@
+
+/*enum name{
+  sujeeth,
+  shalu,
+  devi,
+}
+void main() {
+  print(name.values);
+}*/
+/*enum Data {
+  storage,
+  pendrive,
+  cd,
+  files,
+}
+void main(){
+final Storing1=Store('phone',Data.files);
+final Storing2=Store('laptop',Data.cd);
+Storing1.fn();
+Storing2.fn();
+}
+class Store{
+  final name;
+  final type;
+  Store(this.name,this.type);
+
+
+void fn(){
+  switch(type){
+    case Data.storage:{
+      print("ct");
+    }
+    case Data.cd:{
+      print("cd");
+    }case Data.pendrive:{
+      print("ds");
+  }default:
+    print("something");
+  }
+
+  }
+  }*/
+/*mixin jump{
+  int jumping=10;
+}
+class Animal with jump{
+  void fn(){
+    print(jumping);
+  }
+}
+void main(){
+  final anim=Animal();
+  anim.fn();
+}*/
+
