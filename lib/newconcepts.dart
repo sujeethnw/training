7848675926dde02a394cb9128e376955558c1291
sujeethnw <sujeeth.kumar@nativewit.in>
2024-10-10 11:26:16 +0530

    added new program

--- lib/newconcepts.dart
+++ lib/newconcepts.dart
@@ -109,6 +109,26 @@ class Family extends Relative{
     Relative re=Relative();
     re.father();
 }*/
+/*void main(){
+
+  int i,n=9,flag=0;
+  print("the given num :is $n");
+  for(i=1;i<=n/2;i++){
+    if(n%i==0){
+      flag=1;
+      break;
+    }}
+  if(n==1){
+        print("neither prime nor composite");
+      }else{
+    if(flag==0){
+        print("the given $n is prime ");
+    }else{
+      print("not prime");
+    }
+  }
+  }*/
+
 
 
 
