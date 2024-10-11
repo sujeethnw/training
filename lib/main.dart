d2ee6e4ca62d3709751d47778b58c0cbdeea87bf
sujeethnw <sujeeth.kumar@nativewit.in>
2024-10-11 17:35:25 +0530

    widgets added

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,42 @@
 import 'package:flutter/material.dart';
 
-void main(){
-runApp(MyApp());
+void main() {
+  runApp(MyApp());
 }
-
-class MyApp extends StatelessWidget {
+/*class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Container(
-          height: 100,
-          width: 100,
-          color: Colors.green,
-        ),
+     home:Scaffold(
+       body:Container(
+         height: 100,
+         width: 5,
+         color: Colors.lightBlue,
+       )
+     )
+    );
+  }
+}*/
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child:
+          Column(
+            children: [
+              Text('hi everyone')
+            ],
+            
+          )
       ),
     );
+    
+
+
+
   }
 }
--- lib/widgets.dart
+++
@@ -1,1 +0,0 @@
-import 'pacakge:flutter/material.dart';
