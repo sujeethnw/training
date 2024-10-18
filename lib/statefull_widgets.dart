4a30ec529217013c1f387d0582e2bac0a1d7aeb0
sujeethnw <sujeeth.kumar@nativewit.in>
2024-10-18 11:06:12 +0530

    new

--- lib/statefull_widgets.dart
+++ lib/statefull_widgets.dart
@@ -1,16 +1,2 @@
-import 'login_screen.dart';
 
-class Widgets extends StatefulWidget {
-  const Widgets({super.key});
-
-  @override
-  State<Widgets> createState() => _WidgetsState();
-}
-
-class _WidgetsState extends State<Widgets> {
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
-
+shhijs
